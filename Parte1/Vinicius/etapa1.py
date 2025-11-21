47b56990e417a4c737b9850708ed82441d41e2b8
Vinícius Porto <140714428+viniciusdeoliveiraporto@users.noreply.github.com>
2025-11-20 21:11:24 -0300

    Update etapa1.py

--- Parte1/Vinicius/etapa1.py
+++ Parte1/Vinicius/etapa1.py
@@ -10,11 +10,11 @@ nrz_signal = encode_nrz(test_bits,debug=True)
 print("\n3. Manchester:")
 manchester_signal = encode_manchester(test_bits,debug=True)
 
-#sd.play(manchester_signal, SAMPLE_RATE)
-#sd.wait()
+sd.play(manchester_signal, SAMPLE_RATE)
+sd.wait()
 
 sd.play(nrz_signal, SAMPLE_RATE)
 sd.wait()
 
-#plot_signal(nrz_signal,'NRZ',len(test_bits))
-#plot_signal(manchester_signal,'MANCHESTER',len(test_bits))
+plot_signal(nrz_signal,'NRZ',len(test_bits))
+plot_signal(manchester_signal,'MANCHESTER',len(test_bits))
